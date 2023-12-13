ec79723da5b41a6de89e1a33173e9e96da5143c9
nizwant <mateusznizwantowski@gmail.com>
2023-12-13 09:10:52 +0100

    commit with comments contemplating more optimal solution

--- day_12/task_2.py
+++ day_12/task_2.py
@@ -7,16 +7,16 @@ with open("day_12/input.txt", "r") as file:
 def backtrack(index_list_q_m, list_of_lengths, sequence):
     if not index_list_q_m:
         rec = calculate(sequence)
-        return rec == list_of_lengths
+        return 1  # rec == list_of_lengths
 
-    acc = 0
+    acc = 1  # 0 counts the backtrack invocations
     sequence = sequence[: index_list_q_m[0]] + "#" + sequence[index_list_q_m[0] + 1 :]
-    print(sequence[: index_list_q_m[0] + 1])
     li = calculate(sequence[: index_list_q_m[0] + 1])
     if len(li) < 1 or (
         len(li) <= len(list_of_lengths)
         and li[-1] <= list_of_lengths[len(li) - 1]
         and li[:-1] == list_of_lengths[: len(li) - 1]
+        # we checked previous elements earlier, maybe only tail needs to match
     ):
         acc += backtrack(index_list_q_m[1:], list_of_lengths, sequence)
     sequence = sequence[: index_list_q_m[0]] + "." + sequence[index_list_q_m[0] + 1 :]
@@ -30,16 +30,15 @@ def backtrack(index_list_q_m, list_of_lengths, sequence):
     if len(li) < 1 or (
         len(li) <= len(list_of_lengths)
         and li[-1] <= list_of_lengths[len(li) - 1]
-        and li[:-1]
-        == list_of_lengths[
-            : len(li) - 1
-        ]  # ist almost the same calculation as in line 18
+        and li[:-1] == list_of_lengths[: len(li) - 1]
+        # ist almost the same calculation as in line 18
     ):
         acc += backtrack(index_list_q_m[1:], list_of_lengths, sequence)
 
     return acc
 
 
+# 29790435
 # 319578
 # 30.11418390274048
 
@@ -59,8 +58,7 @@ for line in data:
     sequence = (sequence + "?") * 1 + sequence
     list_of_lengths = list_of_lengths * 2
     indexes_when_question_is = [i for i, val in enumerate(sequence) if val == "?"]
-    acc = backtrack(indexes_when_question_is, list_of_lengths, sequence)
-    accumulator += acc
+    accumulator += backtrack(indexes_when_question_is, list_of_lengths, sequence)
     print(line)
 
 print(accumulator)
