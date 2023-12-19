5d6ee1b503045a4dbc0c4ba948e3d5e46ea7e5f7
nizwant <mateusznizwantowski@gmail.com>
2023-12-19 23:25:26 +0100

    day_19_task_2 refactor

--- day_19/task_2.py
+++ day_19/task_2.py
@@ -1,27 +1,6 @@
 with open("day_19/input.txt", "r") as f:
     data = f.readlines()
 
-
-class Part:
-    def __init__(self, x_min, x_max, m_min, m_max, a_min, a_max, s_min, s_max):
-        self.x = (x_min, x_max)
-        self.m = (m_min, m_max)
-        self.a = (a_min, a_max)
-        self.s = (s_min, s_max)
-        self.list_ordered = [x_min, x_max, m_min, m_max, a_min, a_max, s_min, s_max]
-
-    def __repr__(self):
-        return f"x={self.x}, m={self.m}, a={self.a}, s={self.s}"
-
-    def get_number(self):
-        return (
-            (self.x[1] - self.x[0] + 1)
-            * (self.m[1] - self.m[0] + 1)
-            * (self.a[1] - self.a[0] + 1)
-            * (self.s[1] - self.s[0] + 1)
-        )
-
-
 rules = {}
 was_empty_line = False
 for line in data:
@@ -35,11 +14,16 @@ for line in data:
 
 
 answer = 0
-stack = [(Part(1, 4000, 1, 4000, 1, 4000, 1, 4000), "in")]
+stack = [([1, 4000, 1, 4000, 1, 4000, 1, 4000], "in")]
 while stack:
     parts, start = stack.pop()
     if start == "A":
-        answer += parts.get_number()
+        answer += (
+            (parts[1] - parts[0] + 1)
+            * (parts[3] - parts[2] + 1)
+            * (parts[5] - parts[4] + 1)
+            * (parts[7] - parts[6] + 1)
+        )
         continue
 
     if start == "R":
@@ -49,50 +33,42 @@ while stack:
     for rule in workflow:
         if ":" not in rule:
             stack.append((parts, rule))
-        else:
-            rule, direction = rule.split(":")
-            if rule[0] == "x":
-                val_min, val_max = parts.x[0], parts.x[1]
-                i = 0
-            elif rule[0] == "m":
-                val_min, val_max = parts.m[0], parts.m[1]
-                i = 2
-            elif rule[0] == "a":
-                val_min, val_max = parts.a[0], parts.a[1]
-                i = 4
-            elif rule[0] == "s":
-                val_min, val_max = parts.s[0], parts.s[1]
-                i = 6
+            continue
 
-            if rule[1] == ">":
-                if val_min > int(rule[2:]):
-                    stack.append((parts, direction))
-                    break
-                elif val_max <= int(rule[2:]):
-                    continue
-                else:
-                    a = parts.list_ordered.copy()
-                    a[i] = int(rule[2:]) + 1
-                    stack.append((Part(*a), direction))
+        rule, direction = rule.split(":")
+        if rule[0] == "x":
+            i = 0
+        elif rule[0] == "m":
+            i = 2
+        elif rule[0] == "a":
+            i = 4
+        elif rule[0] == "s":
+            i = 6
 
-                    b = parts.list_ordered.copy()
-                    b[i + 1] = int(rule[2:])
-                    parts = Part(*b)
+        val_min, val_max = parts[i], parts[i + 1]
+        if rule[1] == ">":
+            if val_min > int(rule[2:]):
+                stack.append((parts, direction))
+                break
+            elif val_max <= int(rule[2:]):
+                continue
+            else:
+                a = parts.copy()
+                a[i] = int(rule[2:]) + 1
+                stack.append((a, direction))
+                parts[i + 1] = int(rule[2:])
 
-            elif rule[1] == "<":
-                if val_max < int(rule[2:]):
-                    stack.append((parts, direction))
-                    break
-                elif val_min >= int(rule[2:]):
-                    continue
-                else:
-                    a = parts.list_ordered.copy()
-                    a[i + 1] = int(rule[2:]) - 1
-                    stack.append((Part(*a), direction))
-
-                    b = parts.list_ordered.copy()
-                    b[i] = int(rule[2:])
-                    parts = Part(*b)
+        elif rule[1] == "<":
+            if val_max < int(rule[2:]):
+                stack.append((parts, direction))
+                break
+            elif val_min >= int(rule[2:]):
+                continue
+            else:
+                a = parts.copy()
+                a[i + 1] = int(rule[2:]) - 1
+                stack.append((a, direction))
+                parts[i] = int(rule[2:])
 
 
 print(answer)
